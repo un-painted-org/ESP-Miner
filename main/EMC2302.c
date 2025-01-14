a2d2ae0b9b3dc496ac89502ea67384d833be3be9
unpainted <unpainted.mail@proton.me>
2025-01-14 22:55:41 +0100

    troubleshooting fan rpm readings

--- main/EMC2302.c
+++ main/EMC2302.c
@@ -50,10 +50,10 @@ uint16_t EMC2302_get_fan_speed(uint8_t devicenum)
     ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle,TACH_LSB_REG, &tach_lsb, 1));
     ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle,TACH_MSB_REG, &tach_msb, 1));
 
-    //ESP_LOGI(TAG, "Raw Fan Speed[%d] = %02X %02X", devicenum, tach_msb, tach_lsb);
+    ESP_LOGI(TAG, "Raw Fan Speed[%d] = %02X %02X", devicenum, tach_msb, tach_lsb);
     RPM = (tach_msb << 5) + ((tach_lsb >> 3) & 0x1F);
     RPM = EMC2302_FAN_RPM_NUMERATOR / RPM;
-    //ESP_LOGI(TAG, "Fan Speed[%d] = %d RPM", devicenum, RPM);
+    ESP_LOGI(TAG, "Fan Speed[%d] = %d RPM", devicenum, RPM);
 
     return RPM;
 }
