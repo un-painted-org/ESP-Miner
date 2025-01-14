759e116fffb73d8a6ce184d531910776bbff6b6c
unpainted <unpainted.mail@proton.me>
2025-01-14 23:47:17 +0100

    troubleshooting fan config register

--- main/EMC2302.c
+++ main/EMC2302.c
@@ -50,10 +50,17 @@ uint16_t EMC2302_get_fan_speed(uint8_t devicenum)
     ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle,TACH_LSB_REG, &tach_lsb, 1));
     ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle,TACH_MSB_REG, &tach_msb, 1));
 
-    ESP_LOGI(TAG, "Raw Fan Speed[%d] = %02X %02X", devicenum, tach_msb, tach_lsb);
+    ESP_LOGI(TAG, "Raw Fan Speed[%d] = %02X %02X", devicenum, tach_msb, tach_lsb);  // DEBUG
     RPM = (tach_msb << 5) + ((tach_lsb >> 3) & 0x1F);
     RPM = EMC2302_FAN_RPM_NUMERATOR / RPM;
-    ESP_LOGI(TAG, "Fan Speed[%d] = %d RPM", devicenum, RPM);
+    ESP_LOGI(TAG, "Fan Speed[%d] = %d RPM", devicenum, RPM);                        // DEBUG
+
+    // DEBUG: get fan config
+    uint8_t fan_conf;
+    uint8_t FAN1_CONFIG1 = EMC2302_FAN1_CONFIG1 + (devicenum * 0x10);
+    ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle, FAN1_CONFIG1, &fan_conf, 1));
+    ESP_LOGI(TAG, "Fan config[%d] = %02X", devicenum, fan_conf);
+    // DEBUG
 
     return RPM;
 }
