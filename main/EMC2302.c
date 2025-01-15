3ab1b600108b20464f8f769ca64f2c3b08f013c4
unpainted <unpainted.mail@proton.me>
2025-01-15 16:48:26 +0100

    Bug fix fan controller EMC2302 (#1)
    * troubleshooting fan rpm readings
    
    * troubleshooting emc2302 registers
    
    * troubleshooting fan config register
    
    * setting fan range
    
    * finalized fan config

--- main/EMC2302.c
+++ main/EMC2302.c
@@ -26,6 +26,10 @@ esp_err_t EMC2302_init(bool invertPolarity) {
         ESP_ERROR_CHECK(i2c_bitaxe_register_write_byte(emc2302_dev_handle, EMC2302_PWM_POLARITY, 0b00011111));
     }
 
+    // Set fan range to 00: 500 RPM minimum, TACH count multiplier = 1
+    // fan config default before register write: 2B = 00101011
+    ESP_ERROR_CHECK(i2c_bitaxe_register_write_byte(emc2302_dev_handle, EMC2302_FAN1_CONFIG1, 0b00001011));
+
     return ESP_OK;
 }
 
@@ -50,10 +54,17 @@ uint16_t EMC2302_get_fan_speed(uint8_t devicenum)
     ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle,TACH_LSB_REG, &tach_lsb, 1));
     ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle,TACH_MSB_REG, &tach_msb, 1));
 
-    //ESP_LOGI(TAG, "Raw Fan Speed[%d] = %02X %02X", devicenum, tach_msb, tach_lsb);
+    //ESP_LOGI(TAG, "Raw Fan Speed[%d] = %02X %02X", devicenum, tach_msb, tach_lsb);  // DEBUG
     RPM = (tach_msb << 5) + ((tach_lsb >> 3) & 0x1F);
     RPM = EMC2302_FAN_RPM_NUMERATOR / RPM;
-    //ESP_LOGI(TAG, "Fan Speed[%d] = %d RPM", devicenum, RPM);
+    //ESP_LOGI(TAG, "Fan Speed[%d] = %d RPM", devicenum, RPM);                        // DEBUG
+
+    // DEBUG: get fan config
+    //uint8_t fan_conf;
+    //uint8_t FAN1_CONFIG1 = EMC2302_FAN1_CONFIG1 + (devicenum * 0x10);
+    //ESP_ERROR_CHECK(i2c_bitaxe_register_read(emc2302_dev_handle, FAN1_CONFIG1, &fan_conf, 1));
+    //ESP_LOGI(TAG, "Fan config[%d] = %02X", devicenum, fan_conf);
+    // DEBUG
 
     return RPM;
 }
