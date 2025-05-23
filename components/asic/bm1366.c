bf5c32ea7491b4fe628e8e1e38e507a2e21b3ba8
unpainted <unpainted.mail@proton.me>
2025-05-23 23:37:08 +0200

    debug line

--- components/asic/bm1366.c
+++ components/asic/bm1366.c
@@ -143,6 +143,12 @@ void BM1366_set_version_mask(uint32_t version_mask)
     uint8_t version_byte1 = (versions_to_roll & 0xFF); 
     uint8_t version_cmd[] = {0x00, 0xA4, 0x90, 0x00, version_byte0, version_byte1};
     _send_BM1366(TYPE_CMD | GROUP_ALL | CMD_WRITE, version_cmd, 6, BM1366_SERIALTX_DEBUG);
+
+    printf("New Version Mask: ");
+    for (int i = 0; i < 6; i++) {
+        printf("%02X ", version_cmd[i]);
+    }
+    printf("\r\n");
 }
 
 void BM1366_send_hash_frequency(float target_freq)
